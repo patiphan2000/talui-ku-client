9275f54052edec3821efb1cfdb3e34800cdf04f8
patiphan2000 <patiphan.sri@ku.th>
2021-12-11 22:29:38 +0700

    change footer font color

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { Typography } from '@mui/material';
 
 
 function Footer() {
@@ -15,9 +16,11 @@ function Footer() {
                 <Container maxWidth="lg">
                     <Grid container spacing={5}>
                         <Grid item xs={12}>
-                            <Box>Talui KU client</Box>
+                            <Box>
+                                <Typography color="white">Talui KU client</Typography>
+                            </Box>
                             <Box paddingBottom={3}>
-                                <IconButton href="https://github.com/patiphan2000/talui-ku-client" target="_blank" rel="noreferrer noopener">
+                                <IconButton sx={{ color: "white" }} href="https://github.com/patiphan2000/talui-ku-client" target="_blank" rel="noreferrer noopener">
                                     <GitHubIcon />
                                 </IconButton>
                             </Box>
